ea51fe2215ffe5001e6146a1d9dc12accfb31ea4
tiendat6782 <tiendat56782@gmail.com>
2023-10-18 15:57:18 +0700

    a

--- Back_End/app/helpers/System.php
+++
@@ -1,6 +0,0 @@
-<?php
-function uploadFile($nameFolder, $file){
-    $fileName = time().'_'.$file->getClientOriginalName();
-    return $file->storeAs($nameFolder, $fileName, 'public');
-}
-?>
--- Back_End/routes/web.php
+++ Back_End/routes/web.php
@@ -16,19 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout.index');
 });
-Route::prefix('admin')->group(function () {
-    // Các route bên trong nhóm này sẽ có tiền tố 'admin'
-    Route::get('/product', [App\Http\Controllers\ProductsController::class, 'index'])->name('route_index_product');
-    Route::match(['GET','POST'], '/product/add', [App\Http\Controllers\ProductsController::class, 'addProduct'])->name('route_add_product');
-    Route::match(['GET','POST'], '/product/delete/{id}', [App\Http\Controllers\ProductsController::class, 'deleteProduct'])->name('route_delete_product');
-    Route::match(['GET','POST'], '/product/edit/{id}', [App\Http\Controllers\ProductsController::class, 'editProduct'])->name('route_edit_product');
-    // ...
-});
-Route::prefix('admin')->group(function () {
-    // Các route bên trong nhóm này sẽ có tiền tố 'admin'
-    Route::get('/category', [App\Http\Controllers\CategoryController::class, 'index'])->name('route_index_category');
-    Route::match(['GET','POST'], '/category/add', [App\Http\Controllers\CategoryController::class, 'addCategory'])->name('route_add_category');
-    Route::match(['GET','POST'], '/category/delete/{id}', [App\Http\Controllers\CategoryController::class, 'deleteCategory'])->name('route_delete_category');
-    Route::match(['GET','POST'], '/category/edit/{id}', [App\Http\Controllers\CategoryController::class, 'editCategory'])->name('route_edit_category');
-   
-});
