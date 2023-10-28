37cacf42fffc6142480a8fb94f67cfc8ffd22ba7
lequocphaikql <phailqph20664@fpt.edu.vn>
2023-10-28 15:38:42 +0700

    update products

--- Back_End/resources/views/admin/product/index.blade.php
+++ Back_End/resources/views/admin/product/index.blade.php
@@ -15,15 +15,11 @@
                 </div>            
             </th>
         </thead>
-        
         @isset($products)
             @if ($products->count()>0)
-            @php
-                $i = 1
-            @endphp
                 @foreach ($products as $product)
                     <tr>
-                       <td>{{ $i }}</td>
+                       <td>{{$product->id}}</td>
                        <td>{{$product->name}}</td>
                        <td>{{$product->price}} $</td>
                        <td>{{$product->limitDescription()}}</td>
@@ -31,26 +27,19 @@
                         <img src="{{ asset('storage/'.$product->image) }}" width="200px" alt="">
                         </td>
                        <td>
-                        <a href="{{ route('admin.products.show',['id'=>$product->id]) }}">Chi tiết</a>
-                                           
-                        <td class="fs-3 ">
+                        <a href="">Xem chi tiết</a>
+                       </td>
+                       <td>                      
+                        <div class="text-center">
                             <a href="{{ route('admin.products.edit',['id'=>$product->id]) }}" class="text-warning" ><i class="fa-solid fa-pen-to-square"></i></a>
-                            <a onclick="return confirm('Bạn có muốn xoá sản phẩm này không?')" href="{{ route('admin.products.destroy',['id'=>$product->id]) }}" class="text-danger" ><i class="fa-solid fa-trash"></i></a>
+                        <a onclick="return confirm('Bạn có muốn xoá sản phẩm này không?')" href="{{ route('admin.products.destroy',['id'=>$product->id]) }}" class="text-danger" ><i class="fa-solid fa-trash"></i></a>
                         </div>
                        </td>
                     </tr>
-                    @php
-                        $i++
-                    @endphp
                 @endforeach
             @else
-                    <tr class="text-danger text-center">
-                        <td>No Data</td>
-                    </tr>
+
             @endif
         @endisset
-    </table >
-    <div class="text-center d-flex justify-content-center">
-        {{ $products->links() }}
-    </div>
+    </table>
 @endsection
