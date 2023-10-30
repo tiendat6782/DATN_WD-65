072b0500a71c34e57557cf70b12c6110efc41b05
Your Name <>
2023-10-30 22:26:23 +0700

    fix: Layout website

---
+++ Front_End/src/Components/HomePage.tsx
@@ -0,0 +1,13 @@
+import Header from './Layout/Home/Header';
+import Footer from './Layout/Home/Footer';
+import Banner from './Layout/Home/Banner';
+const HomePage = () => {
+  return (
+    <div>
+        <Header/>
+        <Banner/>
+        <Footer/>
+    </div>
+  )
+}
+export default HomePage
