c60d65dab1324230840d089b8b829340f23f0b57
lequocphaikql <phailqph20664@fpt.edu.vn>
2023-11-15 21:59:53 +0700

    fix api product

--- Back_End/routes/api.php
+++ Back_End/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\ApiProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/product', [ProductController::class, 'index']);
+Route::get('/product', [ApiProductController::class, 'index']);
